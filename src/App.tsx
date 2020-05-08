1fb879118ea91da2223ff5acb17c1d6280d8b356
Tristan Tarpley <tristan@marketr.life>
2020-05-07 19:49:13 -0500

    forgot subdomain

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ let url;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   url = 'http://127.0.0.1:5000/'
 } else {
-  url = 'https://honestdata.world/'
+  url = 'https://api.honestdata.world/'
 }
 
 fetch(url).then(res=> res.json()).then(res=>console.log(res))
