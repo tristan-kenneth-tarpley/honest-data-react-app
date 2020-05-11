8b78b769fd78c541ca41ff72afc686e678664b72
Tristan Tarpley <tristan@marketr.life>
2020-05-11 17:55:12 -0500

    mobile logo and icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,10 +17,13 @@ const Navbar: React.FC = () => {
                         <img src={logo} alt="white-logo.svg"></img>
                     </Link>
                     <div className="btn-group">
-                        <Link className="btn btn-tertiary" to="/about">About honest data</Link>
+                        <Link className="btn btn-tertiary" to="/about">
+                            <i className="fad fa-info"></i>
+                            <span className="hideOnMobile">About honest data</span>
+                        </Link>
                         <Link className="btn btn-tertiary" to="/contact">
                             <i className="fad fa-phone"></i>
-                            Contact
+                            <span className="hideOnMobile">Contact</span>
                         </Link>
                     </div>
                 </div>
